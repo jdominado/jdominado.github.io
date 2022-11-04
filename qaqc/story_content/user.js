ee95d906f8f2cb4f9645967cb3d9caed0b23789d
jdominado <7222055+jdominado@users.noreply.github.com>
2022-11-04 18:19:57 +0800

    upload settings

--- qaqc/story_content/user.js
+++ qaqc/story_content/user.js
@@ -1,7 +1,3 @@
-function ExecuteScript(strId)
-{
-  switch (strId)
-  {
-  }
-}
-
+version https://git-lfs.github.com/spec/v1
+oid sha256:7f2842721eb704c5dd8810dda0221c8f4f87b5743e0e28b9dafe55f0c114c004
+size 67
